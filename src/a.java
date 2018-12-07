ad44ab6d3e4eafdb63905b20922d2c1b2b384cc0
trunglk16 <trungnguyenlk16@gmail.com>
2018-12-07 17:29:00 +0700

    Anh chi test thu thoi

---
+++ src/a.java
@@ -0,0 +1,11 @@
+public class a {
+    private static void main(String[] args) {
+        System.out.println("A" + minValue(10)  );
+    }
+    static int minValue(int x){
+         x = 10;
+         int b = 10;
+
+        return b;
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class test extends a{
+
+
+    public static void main(String[] args) {
+        System.out.println("display "+ a.minValue(10));
+    }
+
+}
+
+
